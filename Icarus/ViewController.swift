f041fb24031c126037419a5f8234c6cb01cd9b72
MacOS <macos@macoss-mac.local>
2019-04-15 07:23:08 -0700

    Initial Commit

---
+++ Icarus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Icarus
+//
+//  Created by MacOS on 15/04/2019.
+//  Copyright © 2019 MacOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
