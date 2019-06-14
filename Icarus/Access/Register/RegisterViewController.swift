e0a09e08cd4f6b1a7021944c45694690c8143f47
Alejandro Caralt Laptop <1998nitai@gmail.com>
2019-06-14 10:43:55 -0700

    Icarus alpha (not stable)

---
+++ Icarus/Access/Register/RegisterViewController.swift
@@ -0,0 +1,49 @@
+//
+//  RegisterViewController.swift
+//  Icarus
+//
+//  Created by MacOS on 23/04/2019.
+//  Copyright © 2019 MacOS. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    // MARK: - Declarations
+    // Var
+    var registerController: RegisterController!
+    
+    // - Register First Step
+    @IBOutlet weak var registerEmailInput: UITextField?
+    
+    // - Register Second Step
+    @IBOutlet weak var registerPasswordInput: UITextField?
+    @IBOutlet weak var registerPasswordRepeatInput: UITextField?
+    
+    // - Register Third Step
+    @IBOutlet weak var registerNameInput: UITextField?
+    @IBOutlet weak var registerCityInput: UITextField?
+    @IBOutlet weak var registerSentenceInput: UITextField?
+    
+    
+
+    // MARK: - ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        registerController = RegisterController.init(viewController: self)
+        
+    }
+    // MARK: - Actions
+    @IBAction func registerEmailButtonTapped(_ sender: UIButton) {
+        registerController.checkEmail()
+    }
+    @IBAction func registerPasswordButtonTapped(_ sender: UIButton) {
+        registerController.checkPassword()
+    }
+    @IBAction func registerRegisterCompleteButtonTapped(_ sender: UIButton) {
+        registerController.tryToRegister()
+    }
+    
+}
